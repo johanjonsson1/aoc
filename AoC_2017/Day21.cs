36ebdef87debf890c9225c3e5e81526c8a61e3db
Johan Jonsson <johan.jonsson@sogeti.se>
2019-11-29 22:29:33 +0100

    some cleanup day21

--- AoC_2017/Day21.cs
+++ AoC_2017/Day21.cs
@@ -3,13 +3,96 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
-using static AoC_Common.SantaHelper;
 
 namespace AoC_2017
 {
     /*
+    --- Day 21: Fractal Art ---
+    You find a program trying to generate some art. It uses a strange process that involves repeatedly enhancing the detail of an image through a set of rules.
 
+    The image consists of a two-dimensional square grid of pixels that are either on (#) or off (.). The program always begins with this pattern:
+
+    .#.
+    ..#
+    ###
+    Because the pattern is both 3 pixels wide and 3 pixels tall, it is said to have a size of 3.
+
+    Then, the program repeats the following process:
+
+    If the size is evenly divisible by 2, break the pixels up into 2x2 squares, and convert each 2x2 square into a 3x3 square by following the corresponding enhancement rule.
+    Otherwise, the size is evenly divisible by 3; break the pixels up into 3x3 squares, and convert each 3x3 square into a 4x4 square by following the corresponding enhancement rule.
+    Because each square of pixels is replaced by a larger one, the image gains pixels and so its size increases.
+
+    The artist's book of enhancement rules is nearby (your puzzle input); however, it seems to be missing rules. 
+    The artist explains that sometimes, one must rotate or flip the input pattern to find a match. (Never rotate or flip the output pattern, though.) 
+    Each pattern is written concisely: rows are listed as single units, ordered top-down, and separated by slashes. For example, the following rules correspond to the adjacent patterns:
+
+    ../.#  =  ..
+              .#
+
+                    .#.
+    .#./..#/###  =  ..#
+                    ###
+
+                            #..#
+    #..#/..../#..#/.##.  =  ....
+                            #..#
+                            .##.
+    When searching for a rule to use, rotate and flip the pattern as necessary. For example, all of the following patterns match the same rule:
+
+    .#.   .#.   #..   ###
+    ..#   #..   #.#   ..#
+    ###   ###   ##.   .#.
+    Suppose the book contained the following two rules:
+
+    ../.# => ##./#../...
+    .#./..#/### => #..#/..../..../#..#
+    As before, the program begins with this pattern:
+
+    .#.
+    ..#
+    ###
+    The size of the grid (3) is not divisible by 2, but it is divisible by 3. It divides evenly into a single square; the square matches the second rule, which produces:
+
+    #..#
+    ....
+    ....
+    #..#
+    The size of this enhanced grid (4) is evenly divisible by 2, so that rule is used. It divides evenly into four squares:
+
+    #.|.#
+    ..|..
+    --+--
+    ..|..
+    #.|.#
+    Each of these squares matches the same rule (../.# => ##./#../...), three of which require some flipping and rotation to line up with the rule. 
+    The output for the rule is the same in all four cases:
+
+    ##.|##.
+    #..|#..
+    ...|...
+    ---+---
+    ##.|##.
+    #..|#..
+    ...|...
+    Finally, the squares are joined into a new grid:
+
+    ##.##.
+    #..#..
+    ......
+    ##.##.
+    #..#..
+    ......
+    Thus, after 2 iterations, the grid contains 12 pixels that are on.
+
+    How many pixels stay on after 5 iterations?
+
+    Your puzzle answer was 117.
+
+    --- Part Two ---
+    How many pixels stay on after 18 iterations?
+
+    Your puzzle answer was 2026963.
     */
 
     public class Day21 : Day
@@ -32,9 +115,9 @@ namespace AoC_2017
                 new Grid(".#./..#/###")
             };
 
-            for (var i = 0; i < 18; i++)
+            for (var i = 0; i < 18; i++) // 5 or 18
             {
-                Console.WriteLine($"Iteration {i+1}");
+                Console.WriteLine($"Iteration {i + 1}");
                 var tempGrids = new List<Grid>();
 
                 foreach (var grid in grids)
@@ -49,15 +132,15 @@ namespace AoC_2017
                     }
 
                     var size = newGrids.First().Size;
-                    var gridsPerSide = (int)Math.Sqrt((double)newGrids.Count);
+                    var gridsPerSide = (int) Math.Sqrt((double) newGrids.Count);
                     var megaPattern = string.Empty;
                     var splits = newGrids.Select(s => s.ToString().Split('/')).ToList();
-                    
+
                     var limiter = 0;
                     var skip = 0;
                     while (limiter < gridsPerSide)
                     {
-                        var currentGrids = splits.Skip(skip).Take(gridsPerSide);
+                        var currentGrids = splits.Skip(skip).Take(gridsPerSide).ToList();
                         for (var j = 0; j < size; j++)
                         {
                             foreach (var currentGrid in currentGrids)
@@ -71,26 +154,26 @@ namespace AoC_2017
                         skip += gridsPerSide;
                         limiter++;
                     }
-                    
+
                     tempGrids.Add(new Grid(megaPattern.TrimEnd('/')));
                 }
 
                 grids = tempGrids;
             }
-            
+
             Console.WriteLine($"Result = {grids.Sum(s => s.PixelsOn)}");
         }
 
         public override void PartTwo()
         {
             base.PartTwo();
+            // See part 1
             Console.WriteLine();
         }
     }
 
     public class Grid
     {
-        //.#./..#/###
         private readonly char[,] grid;
         public char[,] Values => grid;
         public int Size { get; }
@@ -99,13 +182,13 @@ namespace AoC_2017
         public Grid(string pattern)
         {
             var rows = pattern.Split('/');
-            grid = new char[rows.Length,rows.Length];
-            
+            grid = new char[rows.Length, rows.Length];
+
             for (var row = 0; row < rows.Length; row++)
             {
                 for (var col = 0; col < rows.Length; col++)
                 {
-                    grid[row,col] = rows[row][col];
+                    grid[row, col] = rows[row][col];
                 }
             }
 
@@ -134,11 +217,6 @@ namespace AoC_2017
 
         private List<Grid> BreakInto2By2s()
         {
-            //#..#
-            //....
-            //....
-            //#..#
-
             var split = this.ToString().Split('/');
 
             var currentRow = 0;
@@ -154,8 +232,8 @@ namespace AoC_2017
                     pattern += split[currentRow][currentCol];
                     pattern += split[currentRow][currentCol + 1];
                     pattern += "/";
-                    pattern += split[currentRow+1][currentCol];
-                    pattern += split[currentRow+1][currentCol + 1];
+                    pattern += split[currentRow + 1][currentCol];
+                    pattern += split[currentRow + 1][currentCol + 1];
                     grids.Add(new Grid(pattern));
                     currentCol += 2;
                 }
@@ -202,7 +280,7 @@ namespace AoC_2017
 
             return grids;
         }
-         
+
         public Grid GetNewGridByPattern(List<Rule> rules)
         {
             foreach (var pattern in GetFlippedAndRotatedPatterns())
@@ -215,7 +293,7 @@ namespace AoC_2017
                 }
             }
 
-            
+
             throw new Exception("FAIL, should not happen?");
         }
 
