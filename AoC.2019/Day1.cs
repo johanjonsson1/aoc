2db9e4b6a5458450b6fe63ace225ab3a2464cb0f
Johan Jonsson <johan.jonsson@sogeti.se>
2019-11-30 23:38:00 +0100

    2019 preparation

---
+++ AoC.2019/Day1.cs
@@ -0,0 +1,25 @@
+﻿using AoC.Common;
+using System;
+
+namespace AoC2019
+{
+    public class Day1 : Day
+    {
+        public override string Title => "";
+
+        public override void PartOne()
+        {
+            base.PartOne();
+
+            var input = Inputs.Day1.ToStringList();
+
+            Console.WriteLine();
+        }
+
+        public override void PartTwo()
+        {
+            base.PartTwo();
+            Console.WriteLine();
+        }
+    }
+}
---
+++ AoC.2019/Inputs.cs
@@ -0,0 +1,13 @@
+﻿namespace AoC2019
+{
+    public static class Inputs
+    {
+        public const string Day1 = @"";
+
+        public const string Day2 = @"";
+
+        public const string Day3 = @"";
+
+        public const string Day4 = @"";
+    }
+}
