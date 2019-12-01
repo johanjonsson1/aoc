aa67cb2b695645a2dc4711d23a6118b2d55ee7e6
Johan Jonsson <johan.jonsson@sogeti.se>
2019-12-01 22:22:56 +0100

    day2 prep

---
+++ AoC.2019/Day2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AoC.Common;
+using System.Linq;
+using static AoC.Common.SantaHelper;
+
+namespace AoC2019
+{
+    /*
+
+    */
+
+    public class Day2 : Day
+    {
+        public override string Title => "";
+
+        public override void PartOne()
+        {
+            base.PartOne();
+            var input = Inputs.Day2.ToStringList();
+
+
+
+            Console.WriteLine();
+        }
+
+        public override void PartTwo()
+        {
+            base.PartTwo();
+            Console.WriteLine();
+        }
+    }
+}
