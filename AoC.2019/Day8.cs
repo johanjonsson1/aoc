91463e57651e266857f7b522ca89d148833b8996
Johan Jonsson <johan.jonsson@sogeti.se>
2019-12-07 21:55:17 +0100

    cleaned up and added day8

---
+++ AoC.2019/Day8.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using AoC.Common;
+using static AoC.Common.SantaHelper;
+
+namespace AoC2019
+{
+    /*
+
+    */
+
+    public class Day8 : Day
+    {
+        public override string Title => "";
+
+        public override void PartOne()
+        {
+            base.PartOne();
+            var input = Inputs.Day8.ToStringList();
+            input = @"".ToStringList();
+
+
+
+
+
+            Console.WriteLine();
+        }
+
+        public override void PartTwo()
+        {
+            base.PartTwo();
+            Console.WriteLine();
+        }
+    }
+}
