2cc576425c744b9a9532d431c69eb892ebf4b32c
Johan Jonsson <johan.jonsson@sogeti.se>
2019-12-21 15:39:17 +0100

    part2 example working

--- AoC.2019/Day20.cs
+++ AoC.2019/Day20.cs
@@ -157,7 +157,7 @@ namespace AoC2019
             var targetPortal = portals.First(f => f.Name == "ZZ");
             
             var ls = int.MaxValue;
-            GetConnectedTo2(startPortal, targetPortal, startPortal, new List<Portal>() {startPortal},  0, ref ls);
+            ClosestByPortal(startPortal, targetPortal, startPortal, new List<Portal>() {startPortal},  0, ref ls);
 
             Console.WriteLine(ls);
         }
@@ -189,7 +189,7 @@ namespace AoC2019
         }
 
         private void
-            GetConnectedTo2(Portal from, Portal to, Portal last, List<Portal> visited, int count, ref int lowestSteps)
+            ClosestByPortal(Portal from, Portal to, Portal last, List<Portal> visited, int count, ref int lowestSteps)
         {
             if (count > lowestSteps)
             {
@@ -206,7 +206,7 @@ namespace AoC2019
                 {
                     var visited2 = new List<Portal>(visited);
                     visited2.Add(pD.Portal);
-                    GetConnectedTo2(pD.Portal, to, from, visited2, count + pD.Distance, ref lowestSteps);
+                    ClosestByPortal(pD.Portal, to, from, visited2, count + pD.Distance, ref lowestSteps);
                 }
                 else
                 {
@@ -361,11 +361,224 @@ RE....#.#                           #......RF
             }
 
             var startPortal = portals.First(f => f.Name == "AA");
-            var targetPortal = portals.First(f => f.Name == "ZZ");
+            var targetPortal = portals.First(f => f.Name == "ZZ"); 
+            SetInner(portals);
+            //SetLevel(new List<Portal> {startPortal, targetPortal}, 0, portals);
 
             var ls = int.MaxValue;
-            GetConnectedTo2(startPortal, targetPortal, startPortal, new List<Portal>() { startPortal }, 0, ref ls);
+            ClosestByLevel(startPortal, targetPortal, startPortal, new List<string>() { startPortal.GetLevelKey(0) }, 0, 0, ref ls);
+
             Console.WriteLine(ls);
+        }
+
+        private void SetInner(List<Portal> portals)
+        {
+            var maxY = portals.Max(m => m.Coordinate.Y);
+            var maxX = portals.Max(m => m.Coordinate.X);
+            foreach (var portal in portals)
+            {
+                if (portal.Coordinate.Y == 2 || portal.Coordinate.X == 2 || portal.Coordinate.Y == maxY || portal.Coordinate.X == maxX)
+                {
+                    portal.Inner = false;
+                    continue;
+                }
+
+                //if (portal.Coordinate.Y >= 26 && portal.Coordinate.Y <= 82 && portal.Coordinate.X >= 26 &&
+                //    portal.Coordinate.X <= 80)
+                //{
+                //    portal.Inner = true;
+                //}
+
+                portal.Inner = true;
+
+                //    y 27   81
+                //x 27   78
+            }
+        }
+
+        private void SetLevel(List<Portal> portals, int level, List<Portal> all)
+        {
+            portals.ForEach(f =>
+            {
+                f.Level = level;
+                f.ConnectedTo.Where(x => x.Portal.Inner).ToList().ForEach(f1 => f1.Portal.Level = level);
+            });
+
+            foreach (var portal in all)
+            {
+                if (portal.Level == -1)
+                {
+                    portal.Level = 1;
+                }
+            }
+        }
+
+        private static List<string> test = new List<string>() {"AA0",
+            "XF0",
+            "XF1",
+            "CK1",
+            "CK2",
+            "ZH2",
+            "ZH3",
+            "WB3",
+            "WB4",
+            "IC4",
+            "IC5",
+            "RF5",
+            "RF6",
+            "NM6",
+            "NM7",
+            "LP7",
+            "LP8",
+            "FD8",
+            "FD9",
+            "XQ9",
+            "XQ10",
+            "WB10",
+            "WB9",
+            "ZH9",
+            "ZH8",
+            "CK8",
+            "CK7",
+            "XF7",
+            "XF6",
+            "OA6",
+            "OA5",
+            "CJ5",
+            "CJ4",
+            "RE4",
+            "RE3",
+            "IC3",
+            "IC4",
+            "RF4",
+            "RF5",
+            "NM5",
+            "NM6",
+            "LP6",
+            "LP7",
+            "FD7",
+            "FD8",
+            "XQ8",
+            "XQ9",
+            "WB9",
+            "WB8",
+            "ZH8",
+            "ZH7",
+            "CK7",
+            "CK6",
+            "XF6",
+            "XF5",
+            "OA5",
+            "OA4",
+            "CJ4",
+            "CJ3",
+            "RE3",
+            "RE2",
+            "XQ2",
+            "XQ1",
+            "FD1",
+            "FD0"
+        };
+
+        private void
+            ClosestByLevel(Portal from, Portal to, Portal last, List<string> visited, int count, int level, ref int lowestSteps)
+        {
+            if (count > lowestSteps || level < 0 || level > 30 || count > 500)
+            {
+                return;
+            }
+
+            bool OnlyInner(Portal p)
+            {
+
+                if (p.GetLevel(level, @from) > 0 || (p.Inner || p.Name == "ZZ"))
+                {
+                    return true;
+                }
+
+                return false;
+            };
+
+            bool AnotherWayLastTime(Portal p)
+            {
+                var key = p.GetLevelKey(level, from);
+                var i = visited.IndexOf(key);
+
+                if (i == 0)
+                {
+                    return true;
+                }
+
+                if (p.Name == from.Name && !visited[i - 1].StartsWith(p.Name))
+                {
+                    return true;
+                }
+                
+                if (p.Name != from.Name && visited[i-1].StartsWith(p.Name))
+                {
+                    return true;
+                }
+
+                return false;
+            };
+
+            //if (test.Count == visited.Count && !test.Except(visited).Any())
+            //{
+
+            //}
+
+            var visitablePortals = from.ConnectedTo.Where(c => !ReferenceEquals(c.Portal, last) &&
+                                                               //(object.ReferenceEquals(from.Exit, c.Portal) ||
+                                                               (visited.All(
+                                                                    a => a != c.Portal.GetLevelKey(level, @from)) ||
+                                                                AnotherWayLastTime(c.Portal)) &&
+                                                               OnlyInner(c.Portal)
+            ).ToList();
+           
+            foreach (var pD in visitablePortals)
+            {
+                var isTarget = object.ReferenceEquals(pD.Portal, to);
+                var level2 = level;
+
+                if (isTarget && level2 != 0)
+                {
+                    continue;
+                }
+
+                if (!isTarget)
+                {
+                    var visited2 = new List<string>(visited);
+                    //  0     0     1     1     2
+                    // AA -> UW -> UW -> ND -> ND
+                    var sameLevel = from.Name != pD.Portal.Name;
+                    var llevel = -1;
+                    if (sameLevel)
+                    {
+                        llevel = level2;
+                        visited2.Add(pD.Portal.GetLevelKey(level2));
+                    }
+                    else
+                    {
+                        llevel = from.Inner ? level2 + 1 : level2 - 1;
+                        //var inOut = from.Inner ? 'D' : 'U';
+                        visited2.Add(pD.Portal.GetLevelKey(llevel));// + inOut);
+                    }
+                    ClosestByLevel(pD.Portal, to, from, visited2, count + pD.Distance, llevel, ref lowestSteps);
+                }
+                else
+                {
+                    var visited2 = new List<string>(visited);
+                    //var llevel = pD.Portal.Inner ? level2 + 1 : level2 - 1;
+                    visited2.Add(pD.Portal.GetLevelKey(level2));
+                    Console.WriteLine(string.Join(", ", visited2) + " in " + (count + pD.Distance));
+                    if (count + pD.Distance < lowestSteps)
+                    {
+                        lowestSteps = count + pD.Distance;
+                    }
+
+                    //return;
+                }
+            }
         }
 
         private Grid<Coordinate, DonutItem> CreateGrid(List<string> input)
@@ -418,10 +631,39 @@ RE....#.#                           #......RF
         public Coordinate Coordinate;
         public List<PortalDistance> ConnectedTo = new List<PortalDistance>();
         public Portal Exit;
+        public int Level = -1;
+        public bool Inner;
 
         public override string ToString()
         {
             return Name;
+        }
+
+        public string GetLevelKey(int level, Portal p)
+        {
+            if (p.Exit == this)
+            {
+                var llevel = p.Inner ? level + 1 : level - 1;
+                return Name + llevel;
+            }
+
+            return Name + level;
+        }
+
+        public int GetLevel(int level, Portal p)
+        {
+            if (p.Exit == this)
+            {
+                var llevel = p.Inner ? level + 1 : level - 1;
+                return llevel;
+            }
+
+            return level;
+        }
+
+        public string GetLevelKey(int level)
+        {
+            return Name + level;
         }
     }
 
@@ -435,4 +677,11 @@ RE....#.#                           #......RF
             return $"{Distance} steps to {Portal.Name}";
         }
     }
+
+    public class PortalLevel
+    {
+        public int Level;
+        public Portal Portal;
+    }
+
 }
