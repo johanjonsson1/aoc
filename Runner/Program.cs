4a11ee468036fa1a9087d451b334441fa242f0dc
Johan Jonsson <Johan.Jonsson@biometria.se>
2020-12-09 16:58:37 +0100

    day8 unclean

--- Runner/Program.cs
+++ Runner/Program.cs
@@ -7,7 +7,7 @@ namespace Runner
     {
         static void Main()
         {
-            IDay day = new AoC2020.Day7();
+            IDay day = new AoC2020.Day8();
             Console.WriteLine($"Running puzzle {day.Title}");
             day.Run();
 
